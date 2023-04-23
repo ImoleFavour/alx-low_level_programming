2a7f3eedb81a4d05524a76ffb22f3b8e2af972e5
ImoleFavour <imoleokediji@gmail.com>
2023-04-23 06:08:41 -0700

    Puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-/**
-*main - entry point
-*
-*description: 'program is a string'
-*return value of main: 0
-*
-*return: always 0 (success)
-*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
