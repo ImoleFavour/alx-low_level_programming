94b318e5ce8c80932bc94213a2d93b8b9ef80450
ImoleFavour <imoleokediji@gmail.com>
2023-05-02 19:54:13 -0700

    Reverse

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,13 @@
 /**
  * _puts - prints a str, followed by newline, to stdout
  * @str: string to print
+ * Return: _putchar
  */
 void _puts(char *str)
 {
-	while (*str != 0)
+	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
